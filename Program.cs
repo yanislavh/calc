d4190cdb2cc051fd72beb9ca0580fabfea8b0608
IT User <IT User@LAPTOP-3JML8JGG>
2022-10-21 19:10:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver;
+            int i = 0;
+            while(i == 0)
+            {
+                if (Console.ReadLine() == "q" || Console.ReadLine() == "exit")
+                {
+                    i = 1;
+                }
+                else if (Console.ReadLine() == "s" || Console.ReadLine() == "solve")
+                {
+                    Console.Write("Problem: ");
+                    solver = new Solver(Console.ReadLine());
+                }
+            }
+            Environment.Exit(0);
+        }
+        
+    }
+}
---
+++ Solver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calc
+{
+    /*
+     * stepenuvane
+     * korenuwane
+     * log
+     * tan
+     * cos
+     * sin
+     * ln
+     * pi
+     * vid Rad Deg GDR
+     * +
+     * _
+     * :
+     * .
+       
+      */
+    class Solver
+    {
+        string problem;
+        char[] charArr = new char[] { };
+        public Solver(string problem)
+        {
+            this.problem = problem;
+            charArr = problem.ToCharArray();
+        }
+        void Solve()
+        {
+            
+        }
+        void Analize()
+        {
+            for(int i = 0; i < charArr.Length; i++)
+            {
+                if (char[i] == ) ;
+            }
+        }
+    }
+}
