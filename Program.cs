9f6cbac385e45d4d9e1496ac9e00e26433459608
Yanislav Harizanov <yanislav.harizanov@hotmail.com>
2022-10-27 12:54:58 +0300

    neshto

--- Program.cs
+++ Program.cs
@@ -8,18 +8,24 @@ namespace calc
         {
             Solver solver;
             int i = 0;
-            while(i == 0)
-            {
-                if (Console.ReadLine() == "q" || Console.ReadLine() == "exit")
-                {
-                    i = 1;
-                }
-                else if (Console.ReadLine() == "s" || Console.ReadLine() == "solve")
-                {
-                    Console.Write("Problem: ");
-                    solver = new Solver(Console.ReadLine());
-                }
-            }
+            Console.Write("Problem: ");
+            //solver = new Solver(Console.ReadLine());
+            string neshto = "syanislavs";
+            Console.WriteLine(neshto.Substring(1, 8));
+            solver = new Solver("(25+369-19/859)*((15-27)*(19-6)+15*(16-48))");
+            solver.Otvarqshta();
+            //while (i == 0)
+            //{
+            //    if (Console.ReadLine() == "q" || Console.ReadLine() == "exit")
+            //    {
+            //        i = 1;
+            //    }
+            //    else if (Console.ReadLine() == "s" || Console.ReadLine() == "solve")
+            //    {
+            //        Console.Write("Problem: ");
+            //        solver = new Solver(Console.ReadLine());
+            //    }
+            //}
             Environment.Exit(0);
         }
         
--- Solver.cs
+++ Solver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace calc
 {
@@ -24,21 +25,78 @@ namespace calc
     {
         string problem;
         char[] charArr = new char[] { };
+        List<int> otvarqshta = new List<int>();
+        List<int> zatvarqshta = new List<int>();
+        
         public Solver(string problem)
         {
             this.problem = problem;
             charArr = problem.ToCharArray();
         }
-        void Solve()
+        char Ch(string a)
         {
-            
+            return Convert.ToChar(a);
         }
-        void Analize()
+        public void Otvarqshta()
         {
-            for(int i = 0; i < charArr.Length; i++)
+            for (int i = 0; i < charArr.Length; i++)
             {
-                if (char[i] == ) ;
+                if (charArr[i] == Ch("("))
+                {
+                    Zatvarqshta(i);
+                    otvarqshta.Add(i);
+                }
             }
+            CalculiraneNaIndex();
+        }
+        void Zatvarqshta(int i)
+        {
+            int countOtv = 0;
+            int countZatv = 0;
+            for (int j = i; j < charArr.Length; j++)
+            {
+                if (charArr[j] == '(') countOtv++;
+                else if (charArr[j] == ')') countZatv++;
+                if (countOtv == countZatv)
+                {
+                    zatvarqshta.Add(j);
+                    break;
+                }
+
+            }
+        }
+        void CalculiraneNaIndex()
+        {
+            int tempNum = zatvarqshta.Min();
+            int index = zatvarqshta.IndexOf(tempNum);
+            SmqnaReshavane(index);
+        }
+        void SmqnaReshavane(int index)
+        {
+            char[] tempCharArr = new char[] { };
+            string[] chislaIznaci = new string[] { };
+            string minProblem = problem.Substring(otvarqshta[index] + 1, zatvarqshta[index]-otvarqshta[index] - 1);
+            string minproblemzasplit = "";
+            tempCharArr = minProblem.ToCharArray();
+            int tempI = 0;
+            minproblemzasplit.Replace('+', ' ');
+            minproblemzasplit.Replace('-', ' ');
+            minproblemzasplit.Replace('*', ' ');
+            minproblemzasplit.Replace('/', ' ');
+            chislaIznaci = minproblemzasplit.Split(' ');
+            for (int i = 0; i < tempCharArr.Length; i++)
+            {
+                
+                  if(tempCharArr[i] == '+' || tempCharArr[i] == '-' || tempCharArr[i] == '*' || tempCharArr[i] == '/')
+                {
+                    
+                    //chislaIznaci.Add(minProblem.Substring(tempI, i ));
+                    //tempI = i + 1;
+                    //chislaIznaci.Add(tempCharArr[i].ToString());
+                }
+                
+            }
+            //chislaIznaci.Add(minProblem.Substring(tempI));
         }
     }
 }
